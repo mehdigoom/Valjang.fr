628580a10203115e110102f6172c5e084e20ff3a
mehdigoom <medhi.gay@sfr.fr>
2020-09-08 08:45:16 +0200

    Add Lucy Chatbt

---
+++ js/Lucy.js
@@ -0,0 +1,7 @@
+// with ES6 import
+
+//Lucy mesage
+//<article class="msg-container msg-remote" id="msg-0"><div class="msg-box"> <img class="user-img" id="user-0" src="/images/Lucy.jpg" /> <div class="flr"> <div class="messages"> <p class="msg" id="msg-0">Vous avez une question ? Je suis Lucy je suis la pour vous aider.(ou juste papoter )</p></div><span class="timestamp"><span class="username">Lucy</span>&bull;<span class="posttime">3 minutes ago</span></span></div></div></article>
+
+//User message
+//<article class="msg-container msg-self" id="msg-0"><div class="msg-box"><div class="flr"> <div class="messages"><p class="msg" id="msg-1">Messagerie prete !</p></div><span class="timestamp"><span class="username">Usr</span>&bull;<span class="posttime">Now</span></span></div><img class="user-img" id="user-0" src="//gravatar.com/avatar/56234674574535734573000000000001?d=retro" /></div></article>
